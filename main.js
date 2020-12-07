125b85c271bbd9cad84657637032f5014531d514
brandonPerry-Co <Brandonperrymba@gmail.com>
2020-12-07 14:18:04 -0600

    The code works in repl.it but doesn't display the time and date anymore

--- main.js
+++ main.js
@@ -16,11 +16,151 @@ const displayDate = () => {
 
 // Write a JavaScript program to convert a number to a string.
 
+let numTostr = 15
+
+let now = numToStr.toString();
+
+now
+
 
 
 // Write a JavaScript program to convert a string to the number.
 
+let strToNum = "15"
 
+let later = parseInt(strToNum);
+
+later
+
+
+
+
+
+// Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+  // * Boolean
+  let numF = false
+  typeof numF
+  // * Null
+  
+  // * Undefined
+
+  // * Number
+  let numD = 4
+  typeof numD
+  // * NaN
+  let numH = NaN
+  typeof numH
+
+
+  // * String
+  let numG = "this is us"
+  typeof numG
+  
+
+  
+// Write a JavaScript program that adds 2 numbers together.
+
+numA = 30
+numB = 40
+numC = numA + numB
+
+numC
+
+
+
+
+// Write a JavaScript program that runs only when 2 things are true.
+
+let numD = 25
+let numF = 32
+let numG = 25
+if(numD < numF && numD === numG){
+  console.log("bingo")
+}
+
+
+
+// Write a JavaScript program that runs when 1 of 2 things are true.
+
+let numD = 4
+let numF = 32
+let numG = 2
+if(numD < numF || numD === numG){
+  console.log("bingo")
+}
+
+
+
+// Write a JavaScript program that runs when both things are not true.  
+
+let numD = 40
+let numF = 32
+let numG = 2
+if(numD < numF || numD === numG){
+  console.log("bingo")
+}else{
+  console.log("I ran this code instead")
+}
+
+
+
+// ***************************
+//         PART TWO
+// ***************************
+
+// 1. download Live-Server by Ritwick Dey, 
+// 2. reload VS Code, 
+// 3. click the "Go Live" button
+// 4. Go local host 5500 or http://127.0.0.1:5500/index.html to see your web page
+// 5. Or go use the `npm start` command and navigate to localhost:8080 (ctrl + C to close)
+// 6. go to `index.html` 
+// 7. create inputs, buttons and event listeners that render the code blocks you built above to the DOM.
+
+
+
+// Additional Resources
+// Video1: https://player.vimeo.com/video/377147232
+// Video2: https://www.youtube.com/embed/bkvH28PXLWc
+// Video3: https://www.youtube.com/embed/TrGI9Yki-24
+
+
+
+
+
+
+
+
+// **THIS IS INCREDIBLY IMPORTANT THAT YOU DO BOTH SECTIONS!!! You will be doing only front-end work in 421 and you need to brush up on your HTML elements**
+
+
+// ***************************
+//          PART ONE
+// ***************************
+// Write a JavaScript program to display the current day and time, start with:
+console.log(new Date)
+
+const displayDate = () => {
+  const currentDate = new Date()
+
+  document.getElementById("display-element").innerHTML = currentDate;
+}
+ 
+
+// Write a JavaScript program to convert a number to a string.
+
+let numTostr = 15
+
+let now = numToStr.toString();
+
+now
+
+// Write a JavaScript program to convert a string to the number.
+
+let strToNum = "15"
+
+let later = parseInt(strToNum);
+
+later
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
   // * Boolean
